7217c062fd47a23b5b1e5c43bf5d165a4ece4f8b
Vladimir Simonovski <vladimir.v.simonovski@wsa.com>
2021-08-18 15:51:45 +0200

    First successful test

--- src/main/java/model/GetEmployeeResponseModel.java
+++ src/main/java/model/GetEmployeeResponseModel.java
@@ -1,5 +1,6 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GetEmployeeResponseModel {
 
     private List<EmployeeData> data;
